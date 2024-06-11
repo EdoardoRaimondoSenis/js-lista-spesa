a95d7976a6e735e31eb741cfdaefff3a9a413aad
EdoardoRaimondoSenis <166326043+EdoardoRaimondoSenis@users.noreply.github.com>
2024-06-11 14:23:38 +0200

    ora è più carino

--- js/myscript.js
+++ js/myscript.js
@@ -13,8 +13,8 @@ let i = 0;
 
 while (lista[i]) {
     let puntino = lista[i];
-    containerlista.append(puntolista);
     let puntolista = document.createElement("li");
+    containerlista.append(puntolista);
     puntolista.innerHTML = puntino;
     i++
 }
